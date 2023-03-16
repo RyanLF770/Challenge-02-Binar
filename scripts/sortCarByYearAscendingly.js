9aca0ff08dc003532622f4e46a7d821d28954bd8
RyanLF770 <116180735+RyanLF770@users.noreply.github.com>
2023-03-17 03:49:54 +0800

    Update sortCarByYearAscendingly.js

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,1 +1,29 @@
+function sortCarByYearAscendingly(cars) {
+  let years = []
+	cars.map((car) => {
+		if(!years.includes(car.year)) years.push(car.year)
+	})
+	
+	for(let i = 0; i < years.length; i++) {
+		for(let j = i + 1; j < years.length; j++) {
+			if(years[i] > years[j]) {
+				let tmp = years[i]
+				years[i] = years[j]
+				years[j] = tmp
+			}
+		}
+	}
 
+	let mobil = []
+
+  years.map((year) => {
+		cars.map((car) => {
+			if(year == car.year) {
+				mobil.push(car)
+			}
+		})
+	})
+
+  console.log(mobil)
+	return mobil;
+}
